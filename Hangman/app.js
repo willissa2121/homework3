36a3308070743859d508175bd9dff21de88bde93
Scott Willis <scott.willis.wsu@gmail.com>
2019-05-24 22:34:40 -0700

    working psych

--- Hangman/app.js
+++ Hangman/app.js
@@ -1,3 +1,32 @@
-document.getElementById("lose-text").innerHTML="1"
-document.getElementById("guess-text").innerHTML="1"
-document.getElementById("win-text").innerHTML="1"
+let loseGuess= document.getElementById("lose-text")
+let guessAmount=document.getElementById("guess-text")
+let winGuess=document.getElementById("win-text")
+
+let randomNum=7
+let win = 0
+let lose = 0
+let loseAmount = 0
+
+let runEvent=() => {
+
+  console.log(document.getElementById("input").value)
+  let input = document.getElementById("input").value
+  if (input == randomNum){
+    win ++
+    winGuess.innerHTML = win;
+    lose = 0;
+    guessAmount.innerHTML = 10-lose
+  }
+  else if (10 - lose == 1){
+    loseAmount ++;
+    loseGuess.innerHTML = loseAmount;
+    guessAmount.innerHTML = 10
+    lose = 0;
+  }
+  else{
+    lose ++;
+    guessAmount.innerHTML = 10-lose;
+  }
+}
+
+console.log(document.getElementById("input").value)
